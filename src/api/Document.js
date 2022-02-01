4ad02d549f81feb0db962a36c9423487da99ae95
vran <vran_dev@foxmail.com>
2022-02-01 22:22:26 +0800

    feat: support document export

--- src/api/Document.js
+++ src/api/Document.js
@@ -17,3 +17,22 @@ export const getVersionByProjectId =(projectId, parameters) => {
         params: parameters
     })
 }
+
+export const exportDocument = (projectId, params, name, callback) => {
+    return fileDownload(base + "/projects/"+projectId+"/document_files", params, name, callback)
+}
+
+function fileDownload(path, params, name, callback){
+    axios.get(path, {
+      responseType: 'blob', 
+      params: params
+    }).then(response => {
+        let urlObj = window.URL || window.webkitURL || window;
+        const link = document.createElement('a');
+        link.href = urlObj.createObjectURL(new Blob([response]));
+        link.download = name +".md"; //or any other extension
+        document.body.appendChild(link);
+        link.click();
+        callback()
+    }).catch(() => callback())
+  }
